c43f3e8885d698d226a348b134336631e86fbbb5
mayank <123.ultron.1234@gmail.com>
2025-05-23 18:29:40 +0530

    Custom User model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import CustomUser
 
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Social Info', {'fields': ('is_social_user',)}),
+    )
+    list_display = ('email', 'username', 'is_social_user', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_social_user')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
