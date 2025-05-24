4df231cfc32ee2c31c96ba06c89ccdea81de3ce4
mayank <123.ultron.1234@gmail.com>
2025-05-24 17:34:21 +0530

    views updated

--- social/views.py
+++ social/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from accounts.models import Profile
 
 # Create your views here.
 @login_required
 def dashboard_view(request):
 
     
-    return render(request, "profiles/dashboard.html", )
+    try:
+        profile = Profile.objects.get(user = request.user)
+    except Profile.DoesNotExist:
+        profile = None
+
+    return render(request, "profiles/dashboard.html", {'profile':profile})
