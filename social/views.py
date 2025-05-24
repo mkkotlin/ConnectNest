4462d0bb43babf8f493b53bf47e95fec0063874c
mayank <123.ultron.1234@gmail.com>
2025-05-24 16:37:38 +0530

    fixes

--- social/views.py
+++ social/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-
 # Create your views here.
 @login_required
 def dashboard_view(request):
-    return render(request, "profiles/dashboard.html", {"user": request.user})
+    return render(request, "profiles/dashboard.html", )
