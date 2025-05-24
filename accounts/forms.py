92d7df74032c520f58dd6b4afb5c7977a886b1ed
mayank <123.ultron.1234@gmail.com>
2025-05-24 15:49:40 +0530

    forms updated

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,6 +10,19 @@ class CustomUserRegistrationForm(UserCreationForm):
         model = CustomUser
         fields = ('username', 'email', 'password1', 'password2',)
 
+        help_texts = {
+            'username':None,
+            'email':None,
+            'password1':'',
+            'password2': None,
+        }
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field_name in self.fields:
+                self.fields[field_name].help_texts = None
+
+
 class CustomLoginForm(AuthenticationForm):
     username = forms.CharField(label='Username',)
     password = forms.CharField(widget=forms.PasswordInput)
