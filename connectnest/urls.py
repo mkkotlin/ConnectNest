8763387460a876765d43e48ace51220d9f2e716a
mayank <123.ultron.1234@gmail.com>
2025-05-26 13:13:59 +0530

    search app

--- connectnest/urls.py
+++ connectnest/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("social/", include("social.urls")),
     path("", login_view, name="home"),
+    path("search/", include("search.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
