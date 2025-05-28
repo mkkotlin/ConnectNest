30574a723900d072b45d917ad45a9336d8c0b8d8
mayank <123.ultron.1234@gmail.com>
2025-05-28 15:38:19 +0530

    search ui

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from search.views import search
+from search.views import search, filter_s
 
 urlpatterns = [
     path('search/', search, name = 'search'),
+    path('filter_s/', filter_s, name = 'filter_s')
 ]
--- search/views.py
+++ search/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from accounts.models import Profile
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from accounts.models import CustomUser
+from django.db.models import Q
 
 # Create your views here.
 @login_required
@@ -11,3 +14,23 @@ def search(request):
 @login_required
 def result(request):
     profile = Profile.objects.filter(user = request.user)
+
+
+def filter_s(request):
+    q = request.GET.get('q', '')
+    matched_users = CustomUser.objects.filter(Q(username__icontains=q))[:10]
+
+    results = []
+    for user in matched_users:
+        try:
+            profile_pic = user.profile.profile_pic.url  # adjust based on your model
+        except:
+            profile_pic = '/static/default.jpg'  # fallback
+
+        results.append({
+            'username': user.username,
+            'profile_pic': profile_pic,
+            'id':user.id
+        })
+
+    return JsonResponse({'results': results, 'curr_user': request.user.id})
