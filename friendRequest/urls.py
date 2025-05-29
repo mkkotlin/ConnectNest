a8405fc9f55a6f0b462ce91e6d5115bbf5f2c884
mayank <123.ultron.1234@gmail.com>
2025-05-29 16:03:32 +0530

    request sent fixes

--- friendRequest/urls.py
+++ friendRequest/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from friendRequest.views import acceptRequest, rejectRequest, sendFrq
 
 urlpatterns = [
-    path('send/<str:username>/', sendFrq, name='send request'),
-    path('accept/<int:request_id>/', acceptRequest, name = 'accept request'),
-    path('reject/<int:request_id>/', rejectRequest, name = 'reject request')
+    path('send/<str:username>/', sendFrq, name='send_request'),
+    path('accept/<int:request_id>/', acceptRequest, name = 'accept_request'),
+    path('reject/<int:request_id>/', rejectRequest, name = 'reject_request')
 ]
