ccc0eb0421f816b40b3466e49b1e668bd6a87681
mayank <123.ultron.1234@gmail.com>
2025-05-30 15:50:34 +0530

    fixes

--- search/views.py
+++ search/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from accounts.models import CustomUser
 from django.db.models import Q
+from friendRequest.models import FriendRequest
 
 # Create your views here.
 @login_required
@@ -14,23 +15,64 @@ def search(request):
 @login_required
 def result(request):
     profile = Profile.objects.filter(user = request.user)
+    status_f = FriendRequest.objects.filter(user = request.user)
 
+
+# def filter_s(request):
+#     q = request.GET.get('q', '')
+#     matched_users = CustomUser.objects.filter(Q(username__icontains=q))[:10]
+
+#     results = []
+#     for user in matched_users:
+#         try:
+#             profile_pic = user.profile.profile_pic.url  # adjust based on your model
+#         except:
+#             profile_pic = '/static/default.jpg'  # fallback
+
+#         results.append({
+#             'username': user.username,
+#             'profile_pic': profile_pic,
+#             'id':user.id
+#         })
+
+#     return JsonResponse({'results': results, 'curr_user': request.user.id,})
 
 def filter_s(request):
     q = request.GET.get('q', '')
-    matched_users = CustomUser.objects.filter(Q(username__icontains=q))[:10]
+    curr_user = request.user
+    matched_users = CustomUser.objects.filter(Q(username__icontains=q)).exclude(id=curr_user.id)[:10]
 
     results = []
     for user in matched_users:
+        # Default status
+        status = 'none'
+
+        # Check outgoing request from current user to this user
+        outgoing = FriendRequest.objects.filter(from_user=curr_user, to_user=user).first()
+        if outgoing:
+            status = outgoing.status  # pending, accepted, rejected
+        else:
+            # Check incoming request from this user to current user
+            incoming = FriendRequest.objects.filter(from_user=user, to_user=curr_user).first()
+            if incoming:
+                if incoming.status == 'accepted':
+                    status = 'friends'
+                else:
+                    status = incoming.status
+
         try:
-            profile_pic = user.profile.profile_pic.url  # adjust based on your model
+            profile_pic = user.profile.profile_pic.url
         except:
-            profile_pic = '/static/default.jpg'  # fallback
+            profile_pic = '/static/default.jpg'
 
         results.append({
             'username': user.username,
             'profile_pic': profile_pic,
-            'id':user.id
+            'id': user.id,
+            'status': status
         })
 
-    return JsonResponse({'results': results, 'curr_user': request.user.id})
+    return JsonResponse({
+        'results': results,
+        'curr_user': curr_user.id
+    })
