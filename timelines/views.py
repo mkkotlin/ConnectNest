e14db3e5060e3fdb21d5d6b57f45ed3604a66f6c
mayank <123.ultron.1234@gmail.com>
2025-05-31 11:48:10 +0530

    views updated

--- timelines/views.py
+++ timelines/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from timelines.models import PostModel
 
 # Create your views here.
 def timeline(request):
-    return render(request, 'timelines/timeline.html')
+    # post_data = []
+    post_data = PostModel.objects.filter(user = request.user)
+    return render(request, 'timelines/timeline.html', {'post_data':post_data})
