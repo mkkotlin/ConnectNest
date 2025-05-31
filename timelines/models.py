d56aca7168b123a430459a0e8ca12e1c96610f49
mayank <123.ultron.1234@gmail.com>
2025-05-31 07:43:22 +0530

    Like model added

--- timelines/models.py
+++ timelines/models.py
@@ -11,3 +11,12 @@ class PostModel(models.Model):
     def __str__(self):
         return f"{self.user.username}"
     
+class LikeModel(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user','post')
+
+
