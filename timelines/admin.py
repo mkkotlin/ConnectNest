b9b0d6a43b49bc1b288474693a797e80a449bbd7
mayank <123.ultron.1234@gmail.com>
2025-05-31 08:02:28 +0530

    model and admin fixes

--- timelines/admin.py
+++ timelines/admin.py
@@ -6,3 +6,15 @@ from timelines.models import PostModel, CommentModel, LikeModel
 class PostModel(admin.ModelAdmin):
     list_display = ('user', 'content','image', 'created_at',)
     list_filter = ('created_at','user')
+
+
+@admin.register(CommentModel)
+class CommenttModel(admin.ModelAdmin):
+    list_display = ('user', 'post','content', 'created_at',)
+    list_filter = ('created_at','user')
+
+
+@admin.register(LikeModel)
+class LikeModel(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at',)
+    list_filter = ('created_at','post',)
--- timelines/models.py
+++ timelines/models.py
@@ -9,7 +9,7 @@ class PostModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username}"
+        return f"{self.user.username} post_{self.id}"
     
 class LikeModel(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
